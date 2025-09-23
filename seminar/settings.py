316ac2e1b1fece036f0529a0ab4541917b44ae30
hoyah <hoyaho03@snu.ac.kr>
2025-09-23 21:42:44 +0900

    initial setting

--- seminar/settings.py
+++ seminar/settings.py
@@ -31,9 +31,9 @@ environ.Env.read_env(
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# 카카오 로그인 설정
-KAKAO_SECRET_KEY = env('KAKAO_SECRET_KEY')
-KAKAO_REDIRECT_URI = env('KAKAO_REDIRECT_URI')
+
+
+
 KAKAO_PAY_KEY = env('KAKAO_PAY_KEY')
 KAKAO_PAY_CID = env('KAKAO_PAY_CID')
 
