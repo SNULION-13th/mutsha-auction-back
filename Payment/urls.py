187ee90cabb42f377c39a8b57174082af79debc7
teireby <dongin1001@snu.ac.kr>
2025-10-25 16:36:17 +0900

    멋사 3주차 세미나

---
+++ Payment/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PayReadyView, PayApproveView, PayOrderView
+
+app_name = "payment"
+urlpatterns = [
+    # CBV url path
+    path("ready/", PayReadyView.as_view()),
+    path("approve/", PayApproveView.as_view()),
+    path("order/", PayOrderView.as_view()),  # 추가된 경로
+]
