f6ab5687bda1f1f4acccc9ede30243ce623c0ee7
Imggaggu <ggaggu@snu.ac.kr>
2025-10-25 16:31:57 +0900

    [Feat] add payment system

--- UserProfile/urls.py
+++ UserProfile/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import SignUpView, SignInView, SignOutView, TokenRefreshView, UserProfileListView, UserProfileDetailView, CheckUsernameView, RemainingPointDeductView
-
+from .views import SignUpView, SignInView, SignOutView, TokenRefreshView, UserProfileListView, UserProfileDetailView, CheckUsernameView, RemainingPointDeductView,KakaoSignInCallbackView, CheckUsernameView, RemainingPointDeductView
 
 app_name = "UserProfile"
 urlpatterns = [
     # CBV url path
+    path("kakao/callback/", KakaoSignInCallbackView.as_view()),
     path("signup/", SignUpView.as_view()),
     path("signin/", SignInView.as_view()),
     path("signout/", SignOutView.as_view()),
--- seminar/settings.py
+++ seminar/settings.py
@@ -30,6 +30,11 @@ environ.Env.read_env(
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
+# 카카오 로그인 설정
+KAKAO_SECRET_KEY = env('KAKAO_SECRET_KEY')
+KAKAO_REDIRECT_URI = env('KAKAO_REDIRECT_URI')
+KAKAO_PAY_KEY = env('KAKAO_PAY_KEY')
+KAKAO_PAY_CID = env('KAKAO_PAY_CID')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -58,6 +63,7 @@ INSTALLED_APPS = [
     'UserProfile',
     'Point',
     'Auction',
+    'Payment',
 ]
 
 MIDDLEWARE = [
@@ -215,3 +221,4 @@ SWAGGER_SETTINGS = {
         'BearerAuth': []
     }]
 }
+
