3b00a070b09b865ecd417dd5b9bc1bcfb12c8a48
eileen <eileen914@snu.ac.kr>
2025-10-25 16:06:53 +0900

    카카오페이 구현 step1

--- seminar/settings.py
+++ seminar/settings.py
@@ -34,6 +34,9 @@ SECRET_KEY = env('SECRET_KEY')
 KAKAO_SECRET_KEY = env('KAKAO_SECRET_KEY') 
 KAKAO_REDIRECT_URI = env('KAKAO_REDIRECT_URI')
 
+KAKAO_PAY_KEY = env('KAKAO_PAY_KEY')
+KAKAO_PAY_CID = env('KAKAO_PAY_CID')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -61,6 +64,7 @@ INSTALLED_APPS = [
     'UserProfile',
     'Point',
     'Auction',
+    'Payment',
 ]
 
 MIDDLEWARE = [
