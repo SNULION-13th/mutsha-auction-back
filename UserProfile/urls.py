3814286ef5776371a8811d08ce2f22907bff5b79
seatosky2002 <seatosky2002@gmail.com>
2025-10-28 23:45:57 +0900

    Seminar Done

--- UserProfile/urls.py
+++ UserProfile/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import SignUpView, SignInView, SignOutView, TokenRefreshView, UserProfileListView, UserProfileDetailView, CheckUsernameView, RemainingPointDeductView
-
+from .views import SignUpView, SignInView, SignOutView, TokenRefreshView, UserProfileListView, UserProfileDetailView, KakaoSignInCallbackView, CheckUsernameView, RemainingPointDeductView
 
 app_name = "UserProfile"
 urlpatterns = [
     # CBV url path
+    path("kakao/callback/", KakaoSignInCallbackView.as_view()),
+
     path("signup/", SignUpView.as_view()),
     path("signin/", SignInView.as_view()),
     path("signout/", SignOutView.as_view()),
--- seminar/settings.py
+++ seminar/settings.py
@@ -31,6 +31,9 @@ environ.Env.read_env(
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
+KAKAO_SECRET_KEY = env('KAKAO_SECRET_KEY') 
+KAKAO_REDIRECT_URI = env('KAKAO_REDIRECT_URI')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
