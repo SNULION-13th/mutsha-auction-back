1f81b80f422ad012940f6ec60da5ab764e9a5e10
seatosky2002 <seatosky2002@gmail.com>
2025-10-30 22:55:03 +0900

    seminar hw

--- Payment/urls.py
+++ Payment/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import PayApproveView, PayReadyView
+from .views import PayApproveView, PayReadyView, PayOrderView, PaymentHistoryView
 
 app_name = "payment"
 urlpatterns = [
     # CBV url path
     path("ready/", PayReadyView.as_view()),
     path("approve/", PayApproveView.as_view()),
+    path("order/", PayOrderView.as_view()),
+    path("history/", PaymentHistoryView.as_view()),
 ]
