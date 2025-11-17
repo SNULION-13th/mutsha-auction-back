1591822d51bc50dadb593855669424258b3be8d6
lili <suanchoi1102@snu.ac.kr>
2025-11-17 15:17:03 +0900

    5주차 과제

--- seminar/settings.py
+++ seminar/settings.py
@@ -131,7 +131,10 @@ ASGI_APPLICATION = 'seminar.asgi.application'
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
     },
 }
 
