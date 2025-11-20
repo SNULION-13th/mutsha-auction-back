4d9ece9214ccb0c8bd45d7be1d3c00246bb20fb8
moo <lgmoo2002@gmail.com>
2025-11-20 23:57:20 +0900

    feat: 과제5 clear

--- Auction/consumers.py
+++ Auction/consumers.py
@@ -3,9 +3,22 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from asgiref.sync import sync_to_async
 from .models import Auction
 
-# TODO: class NotificationConsumer(AsyncJsonWebsocketConsumer) 
-
+class NotificationConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        self.group_name = "global_notifications"
         
+        await self.channel_layer.group_add(
+            self.group_name, self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.group_name, self.channel_name
+        )
+
+    async def auction_created(self, event):
+        await self.send_json(event["data"])
 class AuctionConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         self.auction_id = self.scope["url_route"]["kwargs"]["auction_id"]
