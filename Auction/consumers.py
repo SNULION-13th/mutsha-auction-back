686f43fb9aa0139786da5ec8099d1f7cb2b35365
cho58 <oke08034@snu.ac.kr>
2025-11-20 18:36:13 +0900

    submission

--- Auction/consumers.py
+++ Auction/consumers.py
@@ -23,3 +23,24 @@ class AuctionConsumer(AsyncJsonWebsocketConsumer):
 
     async def auction_update(self, event):
         await self.send_json(event["data"])
+
+class NotificationConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope["user"]
+        if self.user.is_anonymous:
+            await self.close()
+        else:
+            self.notification_group_name = f"global_notifications"
+
+            await self.channel_layer.group_add(
+                self.notification_group_name, self.channel_name
+            )
+            await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.notification_group_name, self.channel_name
+        )
+
+    async def auction_created(self, event):
+        await self.send_json(event["data"])
