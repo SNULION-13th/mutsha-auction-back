f1803e33e6b4cff0000183d7ef423ec6a23b8423
Imggaggu <ggaggu@snu.ac.kr>
2025-11-20 17:26:43 +0900

    [Feat] add  NotificationConsumer

--- Auction/consumers.py
+++ Auction/consumers.py
@@ -3,7 +3,25 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from asgiref.sync import sync_to_async
 from .models import Auction
 
-# TODO: class NotificationConsumer(AsyncJsonWebsocketConsumer) 
+class NotificationConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope["user"]
+        if self.user.is_anonymous:
+            await self.close()
+        else:
+            self.notifications_group_name = f"notifications_{self.user.id}"
+            await self.channel_layer.group_add(
+                self.notifications_group_name, self.channel_name
+            )
+            await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.notifications_group_name, self.channel_name
+        )
+
+    async def auction_created(self, event):
+        await self.send_json(event["data"])
 
         
 class AuctionConsumer(AsyncJsonWebsocketConsumer):
